878ce200ced26f04510e0ac4c4ccf9f341c0cfaf
Tyler Stapler <tstapler@iastate.edu>
2015-12-19 03:26:22 -0600

    Initial commit of PhotoNow

---
+++ app/src/main/java/staplers/photonow/MainActivity.java
@@ -0,0 +1,13 @@
+package staplers.photonow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
